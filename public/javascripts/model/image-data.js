fa0d8121e5e0e62d8586d755e81436a152e46ecd
Yeon Seo Woo <yeon@Yeonui-MacBook-Pro.local>
2019-09-17 18:26:16 +0900

    [ADD][#1] 미니 케로셀 데이터 추가, 모델객체 생성
    미니케로셀 구현을 위한 필요한 임의 데이터 추가 및 데이터 객체 생성

---
+++ public/javascripts/model/image-data.js
@@ -0,0 +1,8 @@
+export const imgData = [
+	'http://via.placeholder.com/400x150/3498db/fff&text=1',
+	'http://via.placeholder.com/400x150/3498db/fff&text=2',
+	'http://via.placeholder.com/400x150/3498db/fff&text=3',
+	'http://via.placeholder.com/400x150/3498db/fff&text=4',
+	'http://via.placeholder.com/400x150/3498db/fff&text=5',
+	'http://via.placeholder.com/400x150/3498db/fff&text=6',
+];
