68a71720f9a11fb5dd93e3117ff3ed81427c9d1e
Yeon. w <vantovan7414@gmail.com>
2019-09-25 21:47:43 +0900

    [ADD][#10] 아이템 업로드 쿼리 생성

--- db/query/carouselinfo.js
+++ db/query/carouselinfo.js
@@ -1,6 +1,6 @@
 
 const carouselinfo ={
-    SELECT_FILES : `select count(0) as cnt from userinfo where user_id = ?`,
+    INSERT_MAIN_CAROUSEL_CONTENT : `insert into main_carousel(file_seq,title,head,body,link,register,reg_dtm,group_id) values (?,?,?,?,?,?,?,?)`,
     INSERT_MAIN_CAROUSEL_IMG : `insert into attatched_files(dir,filename,extension,register,reg_dtm,size) values (?,?,?,?,?,?)`,
     SELECT_FILE_ID : `select file_seq from attatched_files where dir = ?`,
 }
