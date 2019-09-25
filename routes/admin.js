9672ee9e548a44138808ed5b35be8a7b5602e578
Yeon. w <vantovan7414@gmail.com>
2019-09-25 22:03:45 +0900

    [DEL][#10] 케로셀 등록 api 이동
    route/carousel.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,69 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const multer = require('multer');
-const {uploadCarouselService} = require('../service/uploadCarouselService.js');
-let carouselService = new uploadCarouselService();
+
+const {CarouselController} = require('./controller/carouselController.js');
+const {CarouselService} = require('../service/CarouselService.js');
+let carouselService = new CarouselService();
+let carousel = new CarouselController(carouselService);
+
 /* /admin/main */
 router.get('/main', (req, res)=>{
-  res.render('../public/views/admin/main.ejs');
+    res.render('../public/views/admin/main.ejs');
+  });
+
+router.get('/carousel/uploadform', (req, res)=>{
+  res.render('../public/views/admin/admin-carousel-upload.ejs');
 });
 
-
-
-const storage = multer.diskStorage({
-
-    destination(req, file, callback) {
-        callback(null, 'uploads'); // 기본 업로드  env에 빼자
-    },
-
-    filename(req, file, callback) {
-        let savedName = "";
-        let array = file.originalname.split('.');
-        console.log(array)
-        savedName = Date.now().toString()+"."+array[1];
-        callback(null, savedName);
-    }
-});
-
-const upload = multer({
-    storage,
-    limits: {
-        files: 10,
-        fileSize: 1024 * 1024 * 1024
-    }
-});
-
-router.post('/upload/mini', upload.array('slide_photo', 1), async (req, res, next) => {
-   
-    let result = {
-        status : '200',
-        message : '요청 성공',
-        data : {}
-    }; 
-    try {
-        const files = req.files;  
-        if (Array.isArray(files)) {
-            result.data['filename'] = files[0].originalname;
-            result.data['dir'] = '/uploads/'+files[0].filename;
-            result.data['size'] =files[0].size;
-            result.data['extention'] = files[0].mimetype;
-        }
-        // upload  images 
-        console.log("result.data ", result.data);
-        if(!await carouselService.uploadCarouselCard(result.data)){
-            result.status = "400";
-            result.message = "요청실패";
-        }
-        //let {file_seq} = await carouselService.uploadCarouselContent(result.data.dir);
-        console.log("fileid" ,file_seq);
-        //upload contents
-        console.log(req.body.description);
-        
-        // return result 
-        res.json(result);
-        
-    } catch (err) {
-        console.dir(err.stack);
-    }
-});
 module.exports = router;
