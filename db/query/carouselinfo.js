6d1d301b6c06347ec966f3333da42b12b9496ff4
Yeon. w <vantovan7414@gmail.com>
2019-09-26 20:14:29 +0900

    [ADD][#11] 케로셀 조회 쿼리

--- db/query/carouselinfo.js
+++ db/query/carouselinfo.js
@@ -3,6 +3,9 @@ const carouselinfo ={
     INSERT_MAIN_CAROUSEL_CONTENT : `insert into main_carousel(file_seq,title,head,body,link,register,reg_dtm,group_id) values (?,?,?,?,?,?,?,?)`,
     INSERT_MAIN_CAROUSEL_IMG : `insert into attatched_files(dir,filename,extension,register,reg_dtm,size) values (?,?,?,?,?,?)`,
     SELECT_FILE_ID : `select file_seq from attatched_files where dir = ?`,
+    SELECT_CAROUSEL_LIST : `select a.file_seq, a.title, a.head, a.body, a.link, b.dir, b.filename
+                            from main_carousel a, attatched_files b
+                            where a.file_seq = b.file_seq;`,
 }
 
 module.exports = {carouselinfo}; 
