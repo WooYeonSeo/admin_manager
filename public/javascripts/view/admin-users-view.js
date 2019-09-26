1ef710ac232c03cec70904c18a70287fa339d3b7
Yeon. w <vantovan7414@gmail.com>
2019-09-26 17:02:05 +0900

    [ADD][#9] 사용자 권한 수정 api 화면과 연결

--- public/javascripts/view/admin-users-view.js
+++ public/javascripts/view/admin-users-view.js
@@ -18,10 +18,10 @@ export default class userList {
         trItem: `
           <tr>
             <th scope="row">{idx}</th>
-            <td>{id}</td>
+            <td class="user_id" value="{id}">{id}</td>
             <td>{name}</td>
             <td>{email}</td>
-            <td> <input type="checkbox" name="admin" value="Y" {admin}> </td>
+            <td> <input type="checkbox" name="admin" value="{value}" {admin}> </td>
           </tr>
         `
       };
@@ -29,8 +29,35 @@ export default class userList {
 
     bindEvent() {
       // emitter event on 
-      //this.dom.tableBody.addEventListener("click", this.getMoreUsers.bind(this));
+      this.dom.tableBody.addEventListener("click", this.setUserAuthHandler.bind(this),true);
       this.dom.readmorebtn.addEventListener("click", this.getMoreUsersHadnler.bind(this));
+    }
+
+    async setUserAuthHandler(e){
+      e.stopPropagation();
+      
+      let authtype = e.target.getAttribute("value");
+      let userid = e.target.closest('tr').querySelector('.user_id');
+      
+      await this.setUserAuth(authtype , userid.getAttribute("value"));
+    
+    }
+    async setUserAuth(authtype,userid){
+      console.log(authtype)
+      let fetchSetting = {
+        method: 'POST', 
+        headers : {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+        body: JSON.stringify({type:authtype, userid:userid}), 
+     };
+  
+      return fetch('/signin/update/type', fetchSetting).then(res => res.json())
+      .then((response)=>{
+          console.log("response ", response);
+          return response;
+      });
     }
 
     async getMoreUsersHadnler(){
@@ -59,12 +86,14 @@ export default class userList {
       userArr.forEach((user,idx)=>{
         let trTemplate = this.template.trItem;
         trTemplate = trTemplate.replace('{idx}', `${idx}`);
-        trTemplate = trTemplate.replace('{id}', `${user.user_id}`);
+        trTemplate = trTemplate.replace( /{id}/gi, `${user.user_id}`);
         trTemplate = trTemplate.replace('{name}', `${user.user_name}`);
         trTemplate = trTemplate.replace('{email}', `${user.email}`);
         if(user.admin == 1){
+          trTemplate = trTemplate.replace('{value}', `0`);
           trTemplate = trTemplate.replace('{admin}', `checked`);
         }else{
+          trTemplate = trTemplate.replace('{value}', `1`);
           trTemplate = trTemplate.replace('{admin}', ``);
         }
         trlist += trTemplate;
