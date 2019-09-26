af6d7717b2a1e6544a3d5b53e73b2def9b44bcfa
Yeon. w <vantovan7414@gmail.com>
2019-09-26 15:23:50 +0900

    [FIX] 메인 헤더 이름 view로 변경

--- public/javascripts/view/load-view.js
+++ public/javascripts/view/load-view.js
@@ -1,12 +1,12 @@
-import { imgData } from '../model/image-data.js';
-import { cardData } from '../model/card-data.js';
 import ImageModel from '../model/image-model.js';
 import CardModel from '../model/card-model.js';
 import CarouselViewModel from './carousel-view-model.js';
 import CardCarouselViewModel from './card-carousel-view-model.js';
 import Carousel from './carousel-view.js';
 import CardCarousel from './card-carousel.js';
-import mainHeader from './mainHeader.js';
+import mainHeader from './main-header-view.js';
+import { imgData } from '../model/image-data.js';
+import { cardData } from '../model/card-data.js';
 
 class LoadViewMain {
 	constructor() {
