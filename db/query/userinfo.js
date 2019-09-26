9b1751bc0cb76af87409ab8fbc283aeb6016e38e
Yeon. w <vantovan7414@gmail.com>
2019-09-26 16:59:30 +0900

    [FIX][#8] 사용자조회 관리자 컬럼 추가

--- db/query/userinfo.js
+++ db/query/userinfo.js
@@ -5,7 +5,7 @@ const userinfo ={
     SELECT_USER_ACCOUNT : `select count(0) as cnt from userinfo where user_id = ? and password = ?`,
     INSERT_USER_INFO : `insert into userinfo(user_id, user_name, password, birth, gender, email, phone) values (?,?,?,?,?,?,?)`,
     INSERT_USER_TAGS : `insert into interest_list(user_id, interest) values (?,?)`,
-    SELECT_USER_LIST : `select user_id, user_name, birth, gender, email, phone from userinfo order by user_name asc limit ? offset ?`,
+    SELECT_USER_LIST : `select user_id, user_name, birth, gender, email, phone, admin from userinfo order by user_name asc limit ? offset ?`,
     UPDATE_USER_AUTH_TYPE : `update userinfo set admin = ? where user_id = ?`,
 }
 
