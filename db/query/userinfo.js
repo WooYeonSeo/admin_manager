40096771e424e30162a630ba13095e88d698c081
Yeon. w <vantovan7414@gmail.com>
2019-09-26 17:22:25 +0900

    [FIX] 사용자 정보 조회 쿼리에 권한 추가

--- db/query/userinfo.js
+++ db/query/userinfo.js
@@ -1,7 +1,7 @@
 
 const userinfo ={
     SELECT_USER_ID_CNT : `select count(0) as cnt from userinfo where user_id = ?`,
-    SELECT_USER_INFO : `select user_id, user_name, birth, gender, phone email from userinfo where user_id = ?`,
+    SELECT_USER_INFO : `select user_id, user_name, birth, gender, phone, email, admin from userinfo where user_id = ?`,
     SELECT_USER_ACCOUNT : `select count(0) as cnt from userinfo where user_id = ? and password = ?`,
     INSERT_USER_INFO : `insert into userinfo(user_id, user_name, password, birth, gender, email, phone) values (?,?,?,?,?,?,?)`,
     INSERT_USER_TAGS : `insert into interest_list(user_id, interest) values (?,?)`,
